6875c1aff624f42ef5a641fd95114430d726d664
code4plot <57120354+code4plot@users.noreply.github.com>
2019-10-29 09:56:52 +0800

    Create process_ura_data.R

---
+++ real_estate/process_ura_data.R
@@ -0,0 +1,100 @@
+## process_ura_data.R is a script to parse json-formatted file downloaded from URA
+
+library(jsonlite)
+library(plyr)
+
+#parseLine converts the list object from read_json into a data.frame object
+parseLine = function(theList) {
+	results <- cbind(as.data.frame(theList[1:4]),
+			as.data.frame(theList$rental[[1]])
+			)
+	n = length(theList$rental)
+	if(n > 1){
+		for(i in 2:n){
+			results <- rbind.fill(results,
+						cbind(as.data.frame(theList[1:4]),
+							as.data.frame(theList$rental[[i]])
+							)
+						)
+		}
+	}
+	return(results)
+}
+
+#binArea converts binned area to its midpoint
+binArea = function(x){
+	if(length(grep("-",x))){
+		temp = unlist(strsplit(x,"-"))
+		temp = as.numeric(temp)
+		temp = (temp[1] + temp[2])/2
+	} else {
+		temp = gsub("[^0-9]","",x)
+		temp = as.numeric(temp)
+	}
+	return(temp)
+}
+
+#assignQ assigns a given month, x, to its Quarter
+assignQ = function(x){
+	x = as.numeric(x)
+	for(i in 1:4){
+		if(x <= i*3 & x > (i-1)*3){
+			result = paste("Q",as.character(i),sep="")
+		}
+	}
+	return(result)
+}
+
+#parseDate takes mmyy date and returns yyQx, where x is the Quarter
+parseDate = function(x){
+	x = as.character(x)
+	temp = unlist(strsplit(x,""))
+	if(length(temp) == 4){
+		month = paste(temp[1],temp[2], sep = "")
+		year = paste(temp[3],temp[4], sep = "")
+	} else {
+		month = temp[1]
+		year = paste(temp[2],temp[3], sep = "")
+	}
+	Q = assignQ(month)
+	result = paste(year,Q, sep = "")
+	return(result)
+}
+
+#load .json file from URA
+dat = read_json("18-19_v2.json",flatten=F)
+dat = dat$Result
+
+#initiate data.frame object
+rawDat = data.frame(street = character(),
+x = character(),
+project = character(),
+y = character(),
+areaSqft = character(),
+leaseDate = character(),
+propertyType = character(),
+district = character(),
+noOfBedRoom = character(),
+rent = character(),
+stringsAsFactors = F)
+
+#write dat into data.frame object
+for(i in 1:length(dat)){
+	if(length(dat[[i]]) == 5){
+		rawDat = rbind.fill(rawDat,parseLine(dat[[i]]))
+	}
+}
+
+#convert binned area (e.g. 300-400) to its midpoint (e.g. 350)
+rawDat$areaSqft = unlist(lapply(rawDat$areaSqft, binArea))
+
+#add $ psf column to rawDat
+psf = as.numeric(rawDat$rent)/rawDat$areaSqft
+rawDat = cbind(rawDat, psf = psf)
+
+#convert leaseDate (mmyy) to new format with parseDate
+rawDat$leaseDate = unlist(lapply(rawDat$leaseDate, parseDate))
+
+write.csv(rawDat, "raw_data_191028.csv", row.names = F)
+
+
