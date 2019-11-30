ecae540f9cdb508660dcc5f910e556040dcb0947
code4plot <mail4plot@gmail.com>
2019-11-30 11:15:08 +0800

    remove unused files

--- real_estate/rentalPlot/global.r
+++
@@ -1,6 +0,0 @@
-#load raw data
-rawDat = "data/rawDat_191028.csv"
-rawDat = read.csv(rawDat, stringsAsFactors = F)
-
-#subset to analyze condominiums and executive condominiums
-condo = subset(rawDat, propertyType %in% c("Non-landed Properties", "Executive Condominium"))
